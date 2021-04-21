a2a987573bfe985a3b88f1359b1b2a2d191a5ebe
Tiago Nascimento <tiagopn@ci.ufpb.br>
2021-04-21 10:32:05 -0300

    Update Formation.cpp

--- src/Formation.cpp
+++ src/Formation.cpp
@@ -139,19 +139,9 @@ void Formation::callbackUAVNeighbors(const flocking::Neighbors::ConstPtr& neighb
     for (unsigned int i = 0; i < neighbors->num_neighbors; i++) {
       if (neighbors->range[i] <= max_range_) {
         prox_magnitude = Formation::getProximalMagnitude(neighbors->range[i]);
-
-        if (_fixed_heading_) {
-
-          prox_vector_x += prox_magnitude * sin(neighbors->inclination[i]) * cos(neighbors->bearing[i]);
-          prox_vector_y += prox_magnitude * sin(neighbors->inclination[i]) * sin(neighbors->bearing[i]);
-
-        }else {
-
-          prox_vector_x += prox_magnitude * sin(neighbors->inclination[i]) * cos(neighbors->bearing[i]);
-          prox_vector_y += prox_magnitude * sin(neighbors->inclination[i]) * sin(neighbors->bearing[i]);
-
-        }
-
+        
+        prox_vector_x += prox_magnitude * sin(neighbors->inclination[i]) * cos(neighbors->bearing[i]);
+        prox_vector_y += prox_magnitude * sin(neighbors->inclination[i]) * sin(neighbors->bearing[i]);
         prox_vector_z += prox_magnitude * cos(neighbors->inclination[i]);
       }
     }
